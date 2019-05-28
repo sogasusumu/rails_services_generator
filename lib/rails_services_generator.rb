9b88bfd5ee5a972d05b946cde13c36b192ae3cfa
曽我進 <soga@yocto-inc.com>
2019-05-28 18:37:55 +0900

    refine.

--- lib/rails_services_generator.rb
+++ lib/rails_services_generator.rb
@@ -2,9 +2,4 @@ require "rails_services_generator/railtie"
 
 module RailsServicesGenerator
   # Your code goes here...
-  class << self
-    def call
-      puts 'called'
-    end
-  end
 end
--- lib/tasks/rails_services_generator_tasks.rake
+++
@@ -1,4 +0,0 @@
-desc '`rails g controller_with_services`が、利用可能になります。'
-task :rails_services_generator do
-  puts 'ppspppspspppspps'
-end
